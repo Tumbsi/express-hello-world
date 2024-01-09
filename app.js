b78c5a0c00c2ffc9c8edcc5c58641f7451b8d71d
Tumbsi <78088595+Tumbsi@users.noreply.github.com>
2024-01-09 16:12:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello from Render!
-    Testi testi jeps
-  </title>
+    <title>Testi testi jeps </title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
